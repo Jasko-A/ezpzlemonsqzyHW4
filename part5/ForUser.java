c063b5ab7aef8fc0c469d83a0416edb35593d42c
Ez <fshih@ucdavis.edu>
2018-02-12 12:04:31 -0800

    part5
    completed. ezpz

--- part5/ForUser.java
+++ part5/ForUser.java
@@ -1,10 +1,10 @@
 public class ForUser {
 
-	static int flag = 0;
+
 
 	public static int sum1(For r) {
+		int sum = 0;
 		try {
-			int sum = 0;
 			ForIt temp = new ForIt(r);
 			if(temp.hasNext()){
 				while(temp.hasNext()) {
@@ -15,20 +15,29 @@ public class ForUser {
 			else
 				sum = 0;
 
-			return sum;
+				return sum;
 		}
-		catch UsingIteratorPastEndException e{
-			throw new UsingIteratorPastEndException();
+		catch (UsingIteratorPastEndException e){
+			// throw new UsingIteratorPastEndException();
 		}
-		
+		return sum;
 	}
 
 	public static int sum2(For r) {
 		int sum = 0;
-		ForIt temp = new ForIt();
-		try {
-			if temp.next()
+		int flag = 0;  // setting a flag
+		ForIt temp = new ForIt(r);
+
+		while(flag == 0){   // looping through the try and catching until exception happens
+			try {
+				if (flag == 0){
+					sum += temp.next();
+				}
+			} catch (UsingIteratorPastEndException e){
+				flag = 1;
+			}
 		}
-		
+
+		return sum;
 	}
 }
